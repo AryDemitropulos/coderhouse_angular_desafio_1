bce1b8acea51fb8a8a359d859f26836b18208af9
Ary Demitropulos <demitropulosary@gmail.com>
2021-11-18 21:29:45 -0300

    Se muestra en consola una pregunta de multiple choice

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hola Coderhouse desde TS en un solo comando!");
+var readline = require("readline");
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+rl.question("What is your name ? ", function (name) {
+    rl.question("Where do you live ? ", function (country) {
+        console.log("".concat(name, ", is a citizen of ").concat(country));
+        rl.close();
+    });
+});
+rl.on("close", function () {
+    console.log("\nBYE BYE !!!");
+    process.exit(0);
+});
