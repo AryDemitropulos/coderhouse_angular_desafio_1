c92b9f3f603472a3717e2415447b8caff01bb6f8
Ary Demitropulos <demitropulosary@gmail.com>
2021-11-18 21:39:01 -0300

    Se borra prompt porque no es lo que pense que era

--- index.ts
+++ index.ts
@@ -1,17 +1,15 @@
-const readline = require("readline");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+const prompt = require('prompt');
+
+prompt.start();
+
+prompt.get(['username', 'email'], function (err, result) {
+    if (err) { return onErr(err); }
+    console.log('Command-line input received:');
+    console.log('  Username: ' + result.username);
+    console.log('  Email: ' + result.email);
 });
 
-rl.question("What is your name ? ", function(name) {
-    rl.question("Where do you live ? ", function(country) {
-        console.log(`${name}, is a citizen of ${country}`);
-        rl.close();
-    });
-});
-
-rl.on("close", function() {
-    console.log("\nBYE BYE !!!");
-    process.exit(0);
-});
+function onErr(err) {
+    console.log(err);
+    return 1;
+}
