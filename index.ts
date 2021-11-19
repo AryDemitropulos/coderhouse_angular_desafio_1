44b965d932aef8891932a8f1a6fb63b6b2aac7f9
Ary Demitropulos <demitropulosary@gmail.com>
2021-11-18 21:49:34 -0300

    Se crea interface de Question

--- index.ts
+++ index.ts
@@ -1,16 +1,22 @@
 
-const question:string = `Observables help you manage . . . . . . . . data.\nA. Synchronous\nB. Asynchronous\nC. Both asynchronous & synchronous\nD. None of above\n\n`
-const correctAnswer:string = "b"
-const answer:string = "c"
+const question:Question = {
+    prompt: `Observables help you manage . . . . . . . . data.\nA. Synchronous\nB. Asynchronous\nC. Both asynchronous & synchronous\nD. None of above\n\n`,
+    answer:"b"
+}
+const userAnswer = "c"
 
 console.log(question)
-console.log(`Tu respuesta: ${answer}...`)
+console.log(`Tu respuesta: ${userAnswer}...`)
 
-if(correctAnswer == answer){
+if(question.answer== userAnswer){
     console.log("Respuesta Correcta!")
 }else{
     console.log("Respuesta Incorrecta! :'(")
 }
 
+interface Question{
+    prompt:string,
+    answer:string
+}
 
 
